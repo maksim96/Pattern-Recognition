88c58d0b65d5f8450d4ecad9cc0b617a84c16953
Maximilian Thiessen <s6mnthie@uni-bonn.de>
2018-01-24 18:57:26 +0100

    task 4 ready

--- Project03/task3_4.py
+++ Project03/task3_4.py
@@ -1,121 +1,201 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pylab import *
+from time import clock
+
+def tanh(x, w, theta):
+    return np.squeeze(np.tanh(np.tensordot(w, x, axes=(len(w.shape) - 1, 0)) - theta[..., np.newaxis]))
+
+#d/dx tanh = 1 - tanh^2(x)
+def derrTanhW(X, o, y, w, theta):
+    return X*(1-o**2)
 
 
-def perceptron_out(x, theta, w):
-    return 2 * np.exp(-0.5 * np.power(np.dot(w.T, x) - theta, 2)) - 1
+def derrTanhTheta(X, o, y, w, theta):
+    return -(1-o**2)
 
 
-def loss_fn(x, y, theta, w):
-    return 0.5 * np.sum(np.power(perceptron_out(x, theta, w) - y, 2))
+def gaussianRBF(x,w,theta):
+    # vectorized eval function of perceptron, returns a tensor in the shape of w, where the last dimension
+    # contains evaluations for all pairs of parameters.
+    return np.squeeze(2*np.exp(-0.5*np.power(np.tensordot(w,x,axes=(len(w.shape)-1,0)) - theta[...,np.newaxis],2))-1)
 
 
-def loss_fn_deriv(x, y, theta, w):
-    inner_prod_offset = np.sum(np.inner(w.T, x.T), axis=0) - theta
-    y_val = 2 * np.exp(-0.5 * np.power(inner_prod_offset, 2))
-    theta_deriv = -np.sum(np.multiply((y_val - 1 - y), np.multiply(y_val, -inner_prod_offset)), axis=1)
-    w_deriv = -np.sum(np.multiply((y_val - 1 - y), np.multiply(y_val, np.multiply(inner_prod_offset, x))), axis=1)
-    return theta_deriv, w_deriv
+def loss(out, y):
+    return 0.5 * np.sum(np.power(out - y,2),axis=-1)
 
 
-def run_gradient_descend(x, y, eps, max_iter, update_params_fn):
-    # random start values from standard normal distribution
-    theta = np.random.normal()
-    w = np.mat(np.random.normal(size=(2, 1)))
-
-    # store start values for plotting
-    theta0 = theta
-    w0 = w
-
-    error = np.inf
-    iter = 0
-
-    error_list = np.zeros(max_iter)
-
-    # run descend
-    while error > eps and iter < max_iter:
-        error = loss_fn(x, y, theta, w)
-        error_list[iter] = error
-
-        theta_deriv, w_deriv = loss_fn_deriv(x, y, theta, w)
-        theta, w = update_params_fn(x, y, theta, w, theta_deriv, w_deriv)
-        iter += 1
-
-    return theta0, w0, theta, w, error_list[:iter]
+def derrGaussianW(X, o, y, w, theta):
+    return -X*(o+1)*(np.dot(w,X) - theta)
 
 
-# some plotting functions
-
-def show_perceptron_classification(x, y, theta, w, axis):
-    xv, yv = np.meshgrid(np.linspace(-1.5, 1.5, 150), np.linspace(-1.5, 1.5, 150))
-    map = np.vstack((np.ravel(xv), np.ravel(yv)))
-    res = perceptron_out(map, theta, w)
-    res.shape = xv.shape
-    axis.imshow(res, origin='lower', interpolation='none', cmap='RdYlBu', extent=[-1.5, 1.5, -1.5, 1.5], vmin=-3,
-                vmax=2)
-    axis.scatter(x[:, y == 1][0], x[:, y == 1][1], c='steelblue', alpha=0.7)
-    axis.scatter(x[:, y == -1][0], x[:, y == -1][1], c='orange', alpha=0.7)
+def derrGaussianTheta(X, o, y, w, theta):
+    return (o+1)*(np.dot(w,X) - theta)
 
 
-def plot_perceptron_and_error(name, x, y, t0, w0, t, w, error_list):
-    fig, ax = plt.subplots(1, 3)
-    fig.set_size_inches(15, 5)
-    fig.suptitle(name, fontsize=14)
-    show_perceptron_classification(x, y, t0, w0, ax[0])
-    ax[0].set_title("initialization")
-    show_perceptron_classification(x, y, t, w, ax[1])
-    ax[1].set_title("result after {} steps".format(error_list.size))
-    ax[2].plot(error_list)
-    ax[2].set_title("error")
+def EXY(x,y,f,w,theta):
+    return f(x*w[0] + y*w[1] - theta)
 
 
-# ----------------------
-#     PROGRAM START
-# ----------------------
+def applyL2SVMPolyKernel(x, XS, ys, ms, w0, d, b=1.):
+    if x.ndim == 1:
+        x = x.reshape(len(x), 1)
+    k = (b + np.dot(x.T, XS)) ** d
+    return np.sign(np.sum(k * ys * ms, axis=1) + w0)
 
-# import data
+def plot_perceptron(w, theta, f, title=''):
+    x1, x2 = np.meshgrid(np.linspace(-2, 2, 100), np.linspace(-2, 2, 100))
+    map = np.vstack((np.ravel(x1),np.ravel(x2)))
+    Z = f(map, w, theta)#EXY(x1, x2, f, w, theta)
+    Z.shape = x1.shape
 
-x = np.loadtxt('data/xor-X.csv', dtype=np.float, delimiter=',')
-y = np.loadtxt('data/xor-Y.csv', dtype=np.float, delimiter=None)
+    levels = MaxNLocator(nbins=50).tick_values(Z.min(), Z.max())
 
+    axes = plt.gca()
+    axes.set_xlim([-2, 2])
+    axes.set_ylim([-2, 2])
+	
+    plt.contourf(x1, x2, Z, cmap='RdYlBu', vmin=-2.5, vmax=2, levels=levels)
+    plt.scatter(X[0, y > 0], X[1, y > 0], color='C0')
+    plt.scatter(X[0, y < 0], X[1, y < 0], color='C1')
 
-# naive gradient descend
-# ----------------------
+    plt.title(title)
 
-eta_t = 0.005
-eta_w = 0.001
+    plt.savefig(title + '.png')
 
-
-def naive_param_update(x, y, theta, w, theta_deriv, w_deriv):
-    return theta - eta_t * theta_deriv, w - eta_w * w_deriv
-
-
-results = run_gradient_descend(x, y, 3, 1000, naive_param_update)
-plot_perceptron_and_error("naive gradient descend", x, y, *results)
-
-
-# try more sophisticated approach using line search
-# -------------------------------------------------
-
-eta_t_list = np.array((5, 0.5, 0.05, 0.005))
-eta_w_list = np.array((1, 0.1, 0.01, 0.001))
+    plt.figure()
 
 
-def line_search_param_update(x,y, theta, w, theta_deriv, w_deriv):
-    eta_t, eta_w = 0, 0
-    best_error = np.inf
+def plot_svm_classification(X, y, m, d, b):
+    s = np.where(m>0)[0]
+    XS = X[:,s]
+    ys = y[s]
+    ms = m[s]
+    w0 = np.dot(ys,ms)
 
-    # TODO double for loop = bad
-    for nt in eta_t_list:
-        for nw in eta_w_list:
-            error = loss_fn(x, y, theta - nt * theta_deriv, w - nw * w_deriv)
-            if error < best_error:
-                best_error = error
-                eta_t, eta_w = nt, nw
+    x1, x2 = np.meshgrid(np.linspace(-2, 2, 100), np.linspace(-2, 2, 100))
+    gridMatrix = np.column_stack((x1.flatten(), x2.flatten()))
+    Z = applyL2SVMPolyKernel(gridMatrix.T, XS, ys, ms, w0,d,b)
+    Z = np.reshape(Z, ((100,100)))
 
-    return theta - eta_t * theta_deriv, w - eta_w * w_deriv
+    axes = plt.gca()
+    axes.set_xlim([-2, 2])
+    axes.set_ylim([-2, 2])
+
+    plt.contourf(x1, x2, Z, cmap='RdYlBu', title="L2-KernelSVM with d="+str(d), vmin=-3, vmax=3)
+    plt.scatter(X[0, y > 0], X[1, y > 0], color='C0')
+    plt.scatter(X[0, y < 0], X[1, y < 0], color='C1')
+
+    plt.title('L2 Polynomial Kernel SVM with d = ' + str(d))
+
+    plt.savefig('svm.png')
 
 
-results = run_gradient_descend(x, y, 3, 1000, line_search_param_update)
-plot_perceptron_and_error("gradient descend w/ line search", x, y, *results)
+def trainL2SVMPolyKernel(X, y, d, b=1., C=1., T=1000):
+    m, n = X.shape
+    I = np.eye(n)
+    Y = np.outer(y,y)
+    K = (b + np.dot(X.T, X))**d
+    M = Y * K + Y + 1./C*I
+    mu = np.ones(n) / n
+    for t in range(T):
+        eta = 2./(t+2)
+        grd = 2 * np.dot(M, mu)
+        mu += eta * (I[np.argmin(grd)] - mu)
+    return mu
+
+
+def train_perceptron(X, y, f, dfW, dfTheta, use_line_search=False):
+
+    w = np.random.rand(2)*1
+    theta = np.random.rand(1)*1
+
+    init_w = w.copy()
+    init_theta = theta.copy()
+
+    if use_line_search:
+        eta_list = np.logspace(0,5,6,base=0.1)#[1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
+        eta_w_grid, eta_theta_grid = np.meshgrid(eta_list, eta_list)
+
+    i = 0
+    E = np.inf
+    while i < 1000 and E > 2.76:
+        #output vector
+        o = f(X,w,theta)
+
+        #square loss
+        E = loss(o,y)
+
+        eta_w = 0.005
+        eta_theta = 0.001
+
+        gradW = np.sum(dfW(X, o, y ,w, theta)*(o-y), axis=1)
+        gradTheta = np.sum(dfTheta(X, o, y, w, theta)*(o-y))
+
+        # line search
+        if use_line_search:
+            w_grid = w - np.tensordot(eta_w_grid,gradW,axes=0) if use_line_search else w - eta_w_grid * gradW
+            theta_grid = theta - eta_theta_grid*gradTheta
+            out_grid = f(X,w_grid, theta_grid)
+            square_loss = 0.5 * np.sum(np.power(out_grid - y,2),axis=2)
+            cmin = np.unravel_index(square_loss.argmin(),square_loss.shape)
+            eta_w, eta_theta = eta_w_grid[cmin], eta_theta_grid[cmin]
+
+            # bestE = np.inf
+
+            # for nwTemp in eta_list:
+            #     for nthetaTemp in eta_list:
+            #         wTemp = w - nwTemp*gradW
+            #         thetaTemp = theta - nthetaTemp * gradTheta
+            #         # output vector
+            #         o = f(X, wTemp, thetaTemp)
+            #
+            #         # square loss
+            #         E = 0.5 * np.dot(o - y, o - y)
+            #
+            #         if E < bestE:
+            #             bestE = E
+            #             eta_w = nwTemp
+            #             eta_theta = nthetaTemp
+
+        w -= eta_w*gradW
+        theta -= eta_theta*gradTheta
+
+        wOrth = np.dot(np.array([[0, -1], [1, 0]]), w)
+
+        lam = -theta/np.dot(w,w)*w[0]/wOrth[0]
+        b = theta/np.dot(w,w)*w[1] + lam*wOrth[1]
+
+        i += 1
+
+    return E, w, theta, init_w, init_theta
+
+#train perceptron with activation function f (+ derrivatives), choose best of k random inits. plot results
+def perceptron(f, derrFW, derrFTheta, k, use_line_search=False, plot_title='perceptron'):
+    errors = np.zeros(k)
+    params = list()
+    init_params = list()
+    for i in range(k):
+        e, w, theta, init_w, init_theta = train_perceptron(X, y, f, derrFW, derrFTheta, use_line_search = use_line_search)
+        errors[i] = e
+        params.append((w, theta))
+        init_params.append((init_w, init_theta))
+
+    best_idx = np.argmin(errors)
+    plot_perceptron(*init_params[best_idx], f=f, title=plot_title+' (init)')
+    plot_perceptron(*params[best_idx], f=f, title=plot_title+' (best of ' + str(k) + ')')
+
+X = np.genfromtxt('data/xor-X.csv', delimiter=',')
+y = np.genfromtxt('data/xor-y.csv', delimiter=',')
+
+perceptron(tanh,derrTanhW,derrTanhTheta, 1, use_line_search=True, plot_title='Tanh Activation')
+perceptron(gaussianRBF, derrGaussianW, derrGaussianTheta, 10, plot_title='Gaussian Activation')
+
+m = trainL2SVMPolyKernel(X,y,2, b=1)
+plot_svm_classification(X, y, m, 2, 1)
+
 plt.show()
+
+
+
+
